26431523dc5b537e4cfd043d74a682603e8a7cc6
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-09-03 13:55:32 -0500

    successfully wrote comments and title.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace TagEdit
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -50,16 +49,6 @@ namespace TagEdit
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(39, 127);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(307, 388);
-            this.listBox1.TabIndex = 1;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
@@ -135,7 +124,6 @@ namespace TagEdit
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -147,7 +135,6 @@ namespace TagEdit
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using TagEdit.Properties;
 using TagLib.Id3v1;
 using System.IO;
+using System.Text.RegularExpressions;
 using TagLib.Id3v2; 
 using System.Windows.Forms;
 using System.Windows.Documents;
@@ -21,7 +22,12 @@ namespace TagEdit
 
         public Form1()
         {
+          
             InitializeComponent();
+            listView1.Columns.Add("Title");
+            listView1.Columns.Add("Album");
+            listView1.Columns.Add("Path");
+            listView1.View = View.Details;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,8 +42,44 @@ namespace TagEdit
                         var tFile = TagLib.File.Create(file);
                         var info = new FileInfo(file.ToString());
 
-                        MessageBox.Show(info.Name);
-                        MessageBox.Show(tFile.Tag.Album.ToString());
+                        // MessageBox.Show(tFile.Name.Substring(0, 20));
+                        string name = info.Name;
+                        var results = name.Split(new string[] { " - ", ".m4a" }, StringSplitOptions.RemoveEmptyEntries);
+                       
+                       // tFile.Tag.Comment = results[1];
+                        info = results[1];
+                        tFile.Save();
+                        //string pattern = @"[aeiotu]_[a-z]";
+                        //Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+                        //r.Match(results[1].ToString());
+                        //foreach (Match mat in r.Matches(name))
+                        //{
+                        //    mat.Value.Replace("_", "'");
+                        //    MessageBox.Show(mat.Value.Replace("_", "'").ToString());
+                        //    MessageBox.Show(mat.Value);
+                        //   // tFile.Tag.Comment = newMat;
+                        //    //tFile.Save();
+                        //}
+
+
+
+
+                        //tFile.Tag.Album = "Persona 4 OST";
+
+                        // tFile.Tag.Track = 0;
+
+                        //tFile.Tag.Track = Convert.ToUInt32(Convert.ToString(info.Name[16]) + Convert.ToString(info.Name[17]));
+
+                        //MessageBox.Show(info.Name);
+                        //MessageBox.Show(tFile.Tag.Album.ToString());
+                        //ListViewItem item = new ListViewItem(tFile.Tag.Title);
+                        //item.SubItems.Add(tFile.Tag.Album);
+                        //item.SubItems.Add(Path.GetFullPath(info.FullName));
+                        //listView1.Items.Add(item);
+                        //tFile.Save();
+
+                        // listView1.Items.Add(new ListItem { Name = tFile.Tag.Title, Album = tFile.Tag.Album,Value=Path.GetFullPath(info.FullName) });
+
                     }
                 }
             }
@@ -51,6 +93,18 @@ namespace TagEdit
 
         private void button2_Click(object sender, EventArgs e)
         {
+        }
+    }
+
+    public class ListItem
+    {
+        public string Name;
+        public string Album;
+        public string Value;
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 
